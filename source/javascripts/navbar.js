e32c708fce7f1498b08f34dc8df163b7bfffd1a1
serge cleoron <sc21417@gmail.com>
2019-08-26 19:30:48 +0200

    changes after online test

--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -3,7 +3,7 @@
     if ($(document).scrollTop() > 50) {
       $('.navbar-hoo').css("height", 50);
       $('.dropdown-toggle').css("font-size", 20);
-      $('.navbar-hoo span').css("font-size", "2.5rem");
+      $('.navbar-hoo span').css("font-size", "2rem");
     }
     else {
       $('.navbar-hoo').css("height", 90);
