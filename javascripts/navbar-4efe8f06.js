516777057ea7fcd3c02e742ce9bdcaee866e95c3
sc21417@gmail.com <sc21417@gmail.com>
2019-11-05 11:39:33 +0100

    Automated commit at 2019-11-05 10:39:33 UTC by middleman-deploy 1.0.0

---
+++ javascripts/navbar-4efe8f06.js
@@ -0,0 +1,1 @@
+$(window).scroll(function(){$(document).scrollTop()>50?($(".navbar-hoo").css("height",50),$(".dropdown-toggle").css("font-size",20),$(".navbar-hoo span").css("font-size","2rem")):($(".navbar-hoo").css("height",90),$(".dropdown-toggle").css("font-size",30),$(".navbar-hoo span").css("font-size","3.5rem"))});
